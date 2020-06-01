c23a510b4d852c08aceba2516ed1ef4e737c4a52
kuincan <runningmanzzz@163.com>
2020-06-01 19:03:22 +0800

    第一版内容，没有加实时变化图，账单功能没有完善

--- web/js/AirCondSet-1.0.js
+++ web/js/AirCondSet-1.0.js
@@ -1,34 +1,57 @@
-var urlTest = "https://mockapi.eolinker.com/1hywENt9b4bf5cdb7381fe38284a05d44d0631cf253c095"
-var roomId = 8
-
-//1. 获取并校验调温请求
-//var targetT = $("#targetTemperature").val();
-var targetT = document.getElementById("targetTemperature").value;
-if (targetT < 18 || targetT > 30 ) {
-    $("#checkTemp").html("温度设置出错，请重新设置，范围应在18-30℃");
-    $("#targetTemperature").val("");
-}
-
-//2. 获取调风数据
-var wind = $("input[name='wind']:checked").val();
-
-//3. 获取选择模式
-var mode = $("input[name='mode']:checked").val();
-
-//4. 发送调温调风请求
-var cmdAdd = "/add?"
-function affirm() {
+$(document).ready(function () {
+    var urlTest = "https://mockapi.eolinker.com/1hywENt9b4bf5cdb7381fe38284a05d44d0631cf253c095";
+    var cmdAdd = "/add?"
+    var query = "/query?"
+    var roomId;
     $.ajax({
-        type: "POST",
-        url: urlTest+cmdAdd+roomId+"&mode="+mode+"&wind="+wind+"&targetTemperature="+targetT,
-        data: {
-            "roomId": roomId,
-            "targetTemperature": targetT,
+		type: "GET",
+		url: urlTest+query,
+		data: {"roomId" : roomId},
+		dataType: "json",
+		success: function (response) {
+            $("#roomId").text(response.roomId);
+		}
+    });
+    
+    var wind;
+    var mode;
+    var targetT;
+    //1. 获取并校验调温请求
+    $("#targetTemperature").blur(function () { 
+        targetT = $("#targetTemperature").val();
+        if (targetT < 18 || targetT > 30 ) {
+            $("#checkTemp").text("温度设置出错，请重新设置，范围应在18-30℃");
+            $("#targetTemperature").val('');
+        }    
+    });
+
+    //2. 获取调风数据
+    $("#wind").blur(function () {
+        wind = $("input[name='wind']:checked").val();
+    });
+
+    //3. 获取选择模式
+    $("#mode").blur(function () { 
+        mode = $("input[name='mode']:checked").val();        
+    });
+
+    //4. 发送调温调风请求
+    function affirm() {
+        $.ajax({
+            type: "POST",
+            url: urlTest+cmdAdd,
+            data: {
+            "roomId": $("#roomId").text(),
             "wind": wind,
-            "mode": mode
-        },
-        success: function (response) {
-            alert("设置成功");
-        }
-    });            
-}
+            "mode": mode,
+            "targetTemperature": targetT
+            },
+            success: function (response) {
+                alert("设置成功");
+                window.location.href="../AirCondShow-1.0.html";
+            }
+        });            
+    }
+
+
+})
--- web/js/AirCondShow-1.0.js
+++ web/js/AirCondShow-1.0.js
@@ -1,28 +1,22 @@
-var urlTest = "https://mockapi.eolinker.com/1hywENt9b4bf5cdb7381fe38284a05d44d0631cf253c095"
-var roomId = 8
+
 //1. 状态获取
-var query = "/query?roomId="
-$(function () {
+var urlTest = "https://mockapi.eolinker.com/1hywENt9b4bf5cdb7381fe38284a05d44d0631cf253c095"
+var roomId = 8 //var roomId = $('#roomId').text();
+var query = "/query?"
+$(document).ready(function () {
 	$.ajax({
 		type: "GET",
-		url: urlTest+query+roomId,
+		url: urlTest+query,
 		data: {"roomId" : roomId},
-		success: function (data) {
-			var roomId = data.roomId;
-			var status = data.status;
-			var mode = data.mode;
-			var wind = data.wind;
-			var targetTemperature = data.targetTemperature;
-			var temperature = data.temperature;
-			var totalCost = data.totalCost;
-	
-			$("#roomId").val("roomId");
-			$("#status").val("status");
-			$("#mode").val("mode");
-			$("#wind").val("wind");
-			$("#targetTemperature").val("targetTemperature");
-			$("#temperature").val("temperature");
-			$("#totalCost").val("totalCost");
+		dataType: "json",
+		success: function (response) {
+			$("#roomId").text(response.roomId);
+			$("#status").text(response.status);
+			$("#mode").text(response.mode);
+			$("#wind").text(response.wind);
+			$("#targetTemperature").text(response.targetTemperature);
+			$("#temperature").text(response.temperature);
+			$("#totalCost").text(response.totalCost);
 		}
 	});	
 });
@@ -31,11 +25,11 @@ $(function () {
 //在html中写了
 
 //3. 发送关机请求
-var cmdOff = "/takeoff?roomId"
+var cmdOff = "/takeoff?"
 function shutDown(){
 	$.ajax({
 		type: "GET",
-		url: urlTest+cmdOff+roomId, 
+		url: urlTest+cmdOff, 
 		data: {"roomId": roomId},
 		dataType: "json",
 		success: function (response) {
@@ -45,15 +39,17 @@ function shutDown(){
 }
 
 //4. 下载账单  //这个下载的话是直接打印一张图片呢 还是弄成文件呢？
-var cmdBill = "/detail_bill?roomId="
+var cmdBill = "/detail_bill?"
 function downBill(){
 	$.ajax({
 		type: "GET",
-		url: urlTest+cmdBill+roomId,
+		url: urlTest+cmdBill,
 		data: {"roomId": roomId},
-		dataType: "dataType",
-		success: function (data) {
-			alert(data);
+		dataType: "json",
+		success: function (response) {
+			//现在直接弹出来的是JSON格式的数据
+			var bill = JSON.stringify(response);
+			alert(bill);
 		}
 	});
 }
@@ -62,77 +58,14 @@ function downBill(){
 function downDetail(){
 	$.ajax({
 		type: "GET",
-		url: urlTest+cmdBill+roomId,
+		url: urlTest+cmdBill,
 		data: {"roomId": roomId},
-		success: function (data) {
-			alert(data);
+		dataType: "json",
+		success: function (response) {
+			var detailBill = JSON.stringify(response);
+			alert(detailBill);
 		}
 	});
 }
 
 //6. 显示实时计费表
-var myCharts = encharts.init(document.getElementById("realCharts"));
-
-data = [[],[]];
-option = {
-
-	// Make gradient line here
-	visualMap: [{
-		show: false,
-		type: 'continuous',
-		seriesIndex: 0,
-		min: 0,
-		max: 400
-	}, {
-		show: false,
-		type: 'continuous',
-		seriesIndex: 1,
-		dimension: 0,
-		min: 0,
-		max: dateList.length - 1
-	}],
-
-
-	title: [{
-		left: 'center',
-		text: 'Gradient along the y axis'
-	}, {
-		top: '55%',
-		left: 'center',
-		text: 'Gradient along the x axis'
-	}],
-	tooltip: {
-		trigger: 'axis'
-	},
-	xAxis: [{
-		data: dateList
-	}, {
-		data: dateList,
-		gridIndex: 1
-	}],
-	yAxis: [{
-		splitLine: {show: false}
-	}, {
-		splitLine: {show: false},
-		gridIndex: 1
-	}],
-	grid: [{
-		bottom: '60%'
-	}, {
-		top: '60%'
-	}],
-	series: [{
-		type: 'line',
-		showSymbol: false,
-		data: valueList
-	}, {
-		type: 'line',
-		showSymbol: false,
-		data: valueList,
-		xAxisIndex: 1,
-		yAxisIndex: 1
-	}]
-};
-
-myCharts.setOption(option);
-
